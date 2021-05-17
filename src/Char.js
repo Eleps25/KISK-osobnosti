5d60c6b22f139e418b6354041621ce5d3699b4bd
Jakub Mezulanik <mezuja@gmail.com>
2021-05-17 10:14:35 +0200

    zaklad a otaceni

---
+++ src/Char.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./Char.css";
+
+class Char extends Component {
+    constructor(props) {
+        super(props);
+        this.turnHandler = this.turnHandler.bind(this);
+    }
+    turnHandler() {
+        this.props.turn(this.props.id);
+    }
+    render() {
+        let guessPage = <div>
+            <h2>{this.props.name}</h2>
+            <button onClick={this.turnHandler}>Otoč kartu</button>
+        </div>;
+        let infoPage = <div>
+            <h2>{this.props.name}</h2>
+            <h3>{this.props.years}</h3>
+            <ul className="Char-ul">
+                {this.props.wtr.map((info) => (
+                    <li className="Char-li">{info}</li>
+                ))}
+            </ul>
+            <button onClick={this.turnHandler}>Otoč kartu</button>
+        </div>
+        return (
+            <div className="Char">
+                {this.props.page === 1 ? guessPage : infoPage}
+            </div>
+        )
+    }
+}
+
+export default Char;
---
+++ src/CharList.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Char from "./Char";
+import "./CharList.css"
+
+class CharList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chars: [
+                { name: `Turing, Alan`, years: `1912-1954`, wtr: ["Zakladatel moderní informatiky", "Dešifrování nacistických kódu během 2.SV - Enigma", "Turingův stroj", "Turingův test"], page: 1, id: 5 },
+                { name: `Otlet, Paul`, years: `1868-1944`, wtr: ["MDT", "FID", "Dokumentalistika"], page: 1, id: 6 },
+                { name: `Bush, Vannevar`, years: `1890-1974`, wtr: ["Memex", "As We May Think"], page: 1, id: 7 },
+                { name: `Mahen, Jiří`, years: `1882-1939`, wtr: ["Mahenova knihovna Brno", "redaktor Lidových Novin"], page: 1, id: 8 },
+                { name: `Turing, Alan`, years: `1912-1954`, wtr: ["Zakladatel moderní informatiky", "Dešifrování nacistických kódu během 2.SV - Enigma", "Turingův stroj", "Turingův test"], page: 2 },
+                { name: `Otlet, Paul`, years: `1868-1944`, wtr: ["MDT", "FID", "Dokumentalistika"], page: 2 },
+                { name: `Bush, Vannevar`, years: `1890-1974`, wtr: ["Memex", "As We May Think"], page: 2 },
+                { name: `Mahen, Jiří`, years: `1882-1939`, wtr: ["Mahenova knihovna Brno", "redaktor Lidových Novin"], page: 2 },
+            ]
+        };
+        this.turnCard = this.turnCard.bind(this);
+    }
+    turnCard(id) {
+        const newChars = this.state.chars.map(char => {
+            if (char.id === id) {
+                if (char.page === 1) {
+                    return { ...char, page: 2 };
+                }
+                if (char.page === 2) {
+                    return { ...char, page: 1 };
+                }
+            }
+            return char;
+        });
+
+        this.setState({ chars: newChars });
+    }
+    render() {
+        return (
+            <div className="CharList">
+                <h1>Osobnosti informační vědy</h1>
+                <div className="CharList-list">
+                    {this.state.chars.map((char) => (
+                        <Char
+                            name={char.name}
+                            years={char.years}
+                            wtr={char.wtr}
+                            page={char.page}
+                            turn={this.turnCard}
+                            id={char.id}
+                            position={this.state.char}
+                        />
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CharList;
